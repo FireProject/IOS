e469491638c8ae0c4981d0ad673248f3f0cdf6be
DAEHOCHANG <eogh1209@gmail.com>
2021-03-30 14:58:48 +0900

    대략적인 ui완성, 스크롤뷰(혹은 테이블 뷰) 추가및 채팅시 나올 텍스트 만들어야함

--- IOS/Controller/MainService/ChattingViewController.swift
+++ IOS/Controller/MainService/ChattingViewController.swift
@@ -12,23 +12,45 @@ import FirebaseStorage
 import FirebaseDatabase
 
 
-class ChattingViewController:UIViewController {
+class ChattingViewController:UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var messageTextFiled: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageTextFiled.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= (keyboardSize.height - 20
+                )
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.isToolbarHidden = true
         self.navigationController?.navigationBar.backgroundColor = .clear
     
         self.navigationItem.title = "test 방"
-        //self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "SideMenuImage"), style: .plain, target: nil, action: nil)
         
         let menuBtn = UIButton(type: .custom)
         menuBtn.frame = CGRect(x: 0.0, y: 0.0, width: 25, height: 25)
         //menuBtn.setImage(#imageLiteral(resourceName: "SideMenuImage"), for: .normal)
         menuBtn.setBackgroundImage(#imageLiteral(resourceName: "SideMenuImage"), for: .normal)
-
 
         let menuBarItem = UIBarButtonItem(customView: menuBtn)
         let currWidth = menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 25)
@@ -38,8 +60,17 @@ class ChattingViewController:UIViewController {
         self.navigationItem.rightBarButtonItem = menuBarItem
         
         
-        
         self.navigationController?.navigationBar.isHidden = false
         self.navigationController?.navigationBar.topItem?.title = ""
     }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
 }
